1d12a80aeb50602ba3ba1d08b7346a8295f400ae
James Alfei <jamesalfei@gmail.com>
2014-06-09 14:26:23 +0100

    Added IBeacon Code.

--- Android app/src/com/eyerange/app/LoginActivity.java
+++ Android app/src/com/eyerange/app/LoginActivity.java
@@ -5,11 +5,17 @@ package com.eyerange.app;
 
 import networkUtilities.NetworkCommsFeedback;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 /**
- * @author James
+ * This is the first screen the user will see. This class logs the user into the
+ * server (for authentication) and then switches to the main activity
  * 
+ * this will eventually need to check if the user is already logged in and go
+ * straight to the main activity
+ * 
+ * @author James
  */
 public class LoginActivity extends Activity implements NetworkCommsFeedback {
 
@@ -23,24 +29,21 @@ public class LoginActivity extends Activity implements NetworkCommsFeedback {
 	}
 
 	private void setupActionListeners() {
-		// TODO Auto-generated method stub
-
+		// add button action listeners here
 	}
 
 	private void setupInterfaceComponents() {
-		// TODO Auto-generated method stub
-
+		// setup all interface components here
 	}
 
 	@Override
 	public void onLoginComplete(boolean success, boolean honbuUser, String token) {
-		// TODO Auto-generated method stub
-
+		// when logged in, go to Main activity
+		startActivity(new Intent(this, MainActivity.class));
 	}
 
 	@Override
 	public void onLogoutComplete(boolean success) {
 		// TODO Auto-generated method stub
-
 	}
 }

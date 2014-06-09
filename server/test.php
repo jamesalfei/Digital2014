49eccf416b21b27dc28109b7f58de06622818df3
robrotheram <robertfletcher@Roberts-MacBook-Air.local>
2014-06-09 15:18:09 +0100

    Added a login check - demo only
    Added check token function
    test.php has forms for testing

--- server/test.php
+++ server/test.php
@@ -1,14 +1,20 @@
-<?php
+<html>
+	
+	
+	
+	<form method="post" action="http://eyerange.co.uk/api/check.php">
+		<label>Check</label><input type="text" value="token" name="usrToken" id="usrToken"/>
+		<input type="submit" />
+	</form>
+	
+	<form method="post" action="http://eyerange.co.uk/api/">
+		<label>Login</label>
+		<input type="text" name="content_type" value="content_type" id="usrToken"/>
+		<input type="text" name="request_type" value="request_type" id="usrToken"/>
+		<input type="text" name="username"  value="username" id="usrToken"/>
+		<input type="text" name="password" value="password" id="usrToken"/>
 
-ini_set('display_startup_errors',1);
-ini_set('display_errors',1);
-error_reporting(-1);
-
-
-require_once ('login.php');
-
-echo addLogin('Robert','test');
-echo "<br/>";
-echo "<br/>";
-echo login('Robert','test');
-?>	
+		<input type="submit" />
+	</form>
+	
+</html>

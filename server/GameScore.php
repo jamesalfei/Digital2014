d9c7249cc283415caf191112bfd5b4ed291607a7
robrotheram <robertfletcher@Roberts-MacBook-Air.local>
2014-06-10 05:26:03 +0100

    Added GetMethods for sores.

--- server/GameScore.php
+++ server/GameScore.php
@@ -46,12 +46,63 @@ function addFinalScore($courseID,$score,$tokenw,$weather){
 }
 
 function getYourHoleScores($tokenw,$holeID){
+	$reply = array();
+		session_start();	
+		if($_SESSION['token'] == $tokenw){
+			$reply['response']  = TRUE;
+			$user = $_SESSION['user'];
+			global $con;
+			$sql  = "SELECT * FROM Scores WHERE HoleID = '$holeID' && UserID = '$user'";
+			$resp;
+			if (!mysqli_query($con,$sql)){
+				$resp = FALSE;
+				die('Error: ' . mysqli_error($con));
+			}else{
+				$resp = mysqli_query($con,$sql);
+			}
+			while($row = mysqli_fetch_array($resp)) {
+			  //Add this row to the reply
+		    $response['HoleID'] = $row['HoleID'];
+		    $response['Socre'] = $row['Score'];
+			$response['Date'] = $row['Date'];
+		    $reply['Data'][] = $response;
+			}		
+		} else{
+			$reply['response']  = 'ERROR';
+			$reply['message']  = 'invalid token have you tried to login';
+		}
+		return json_encode($reply);
 
 }
 
 
 function getYourGameScores($tokenw,$CourseID){
-
+	$reply = array();
+	session_start();	
+	if($_SESSION['token'] == $tokenw){
+		$reply['response']  = TRUE;
+		$user = $_SESSION['user'];
+		global $con;
+		$sql  = "SELECT * FROM Game WHERE CourseID = '$CourseID'&& UserID = '$user'";
+		$resp;
+		if (!mysqli_query($con,$sql)){
+			$resp = FALSE;
+			die('Error: ' . mysqli_error($con));
+		}else{
+			$resp = mysqli_query($con,$sql);
+		}
+		while($row = mysqli_fetch_array($resp)) {
+		  //Add this row to the reply
+	    $response['CourseID'] = $row['CourseID'];
+	    $response['Socre'] = $row['Score'];
+		$response['Weather'] = $row['Weather'];
+	    $reply['Data'][] = $response;
+		}		
+	} else{
+		$reply['response']  = 'ERROR';
+		$reply['message']  = 'invalid token have you tried to login';
+	}
+	return json_encode($reply);
 }
 
 
